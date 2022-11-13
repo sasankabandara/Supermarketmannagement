41e44dfb10999bf475a4928fbc4186dbe800300b
Sasanka <71892768+sasankabandara@users.noreply.github.com>
2022-11-13 23:32:12 +0530

    Add files via upload

---
+++ Supermarket Management System In csharp/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Supermarket_Management_System_In_csharp
+{
+    public class DatabaseConnection
+    {
+        private SqlConnection connection;
+        public DatabaseConnection() { 
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\ERP assignment\Supermarket Management System in csharp\Supermarket Management System In csharp\Supermarket Management System In csharp\SMS.mdf"";Integrated Security=True;Connect Timeout=30");
+        }
+
+        public SqlConnection GetSqlConnection() { 
+            return connection;
+        }
+    }
+}
